cb231fba89c829d93551e5339c6b6b3f6a72025b
Igor Sánchez <igortxete7@gmail.com>
2016-12-20 18:08:56 +0100

    Removed

--- TheStrangersCom/dataBase.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$connect = mysqli_connect("mysql.hostinger.es", "u992754564_djs", "Yps-DKp-eHS-7zQ", "u992754564_stran");
-$connect = mysqli_connect("localhost", "root", "", "TheStrangers");
-?>
-
-
-
